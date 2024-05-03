a2290c7a12602b152e5841957f80d55ea4c521f0
Jhin <54132604+Jinseok1025@users.noreply.github.com>
2024-05-03 09:25:48 +0900

    토큰 검증

--- controller/tweet.js
+++ controller/tweet.js
@@ -1,27 +1,32 @@
-import * as tweetRepository from '../data/tweet.js';
+import * as tweetRepository from '../Data/tweet.js';
+
 // 여러 트윗을 가져오는 함수
-export async function getTweets(req, res){
+export async function getTweets(req, res) {
     const username = req.query.username;
-    const data = await (username ? tweetRepository.getAllByUsername(username)
-                                 : tweetRepository.getAll());
+    const data = await (username ? tweetRepository.getAllbyUsername(username)
+                                : tweetRepository.getAll());
     res.status(200).json(data);
 }
+
 // 하나의 트윗을 가져오는 함수
 export async function getTweet(req, res, next) {
     const id = req.params.id;
-    const tweet = await tweetRepository.getById(id);
+    const tweet = await tweetRepository.getbyId(id);
     if(tweet){
         res.status(200).json(tweet);
     }else{
-        res.status(404).json({message: `${id}의 트윗이 없습니다`})
+        res.status(404).json({message: `${id}의 트읫이 없습니다`})
     }
 }
+
 // 트윗을 생성하는 함수
 export async function createTweet(req, res, next) {
     const {text, name, username} = req.body;
+    // body('text').trim().isLength({min:3}).withMessage('텍스트는 세글자 이상으로 입력!')
     const tweet = await tweetRepository.create(text, name, username);
     res.status(201).json(tweet);
-}
+};
+
 // 트윗을 변경하는 함수
 export async function updateTweet(req, res, next) {
     const id = req.params.id;
@@ -30,9 +35,10 @@ export async function updateTweet(req, res, next) {
     if(tweet){
         res.status(201).json(tweet);
     }else{
-        res.status(404).json({message: `${id}의 트윗이 없습니다`})
+        res.status(404).json({message: `${id}의 트읫이 없습니다`})
     }
 }
+
 // 트윗을 삭제하는 함수
 export async function deleteTweet(req, res, next) {
     const id = req.params.id;
--- router/tweets.js
+++ router/tweets.js
@@ -1,37 +1,98 @@
 import express from "express";
-import * as tweetController from '../controller/tweet'
+import * as tweetController from '../controller/tweet.js';
 
 const router = express.Router();
-
+const tweetsRouter = express.Router();
 
 // 해당 아이디에 대한 트윗 가져오기
 // GET
 // http://localhost:8080/tweets?username=:username
-router.get('/', tweetController.getTweets);
+// router.get('/', (req, res, next) => {
+//     const username = req.query.username;
+//     const data = username
+//         ? tweets.filter((tweet) => tweet.username == username)
+//         : tweets;
+//     res.status(200).json(data);
+// });
+
+router.get('/', isAuth, tweetController.getTweets);
 
 
 // 글번호에 대한 트윗 가져오기
 // GET
 // http://localhost:8080/tweets/:id
-router.get('/:id', tweetController.getTweet); 
+// router.get('/:id', (req, res, next) => {
+//     const id = req.params.id;
+//     const tweet = tweets.find((tweet) => tweet.id === id);
+//     if(tweet){
+//         res.status(200).json(tweet);
+//     }else{
+//         res.status(404).json({message: `${id}의 트윗이 없습니다`});
+//     }
+// });
+
+router.get('/', isAuth, tweetController.getTweet);
+
+
 
 
 // 트윗하기
 // POST
 // http://localhost:8080/tweets
 // name, username, text
-// json형태로 입력 후 추가된 데이터까지 모두 json으로 출력
-router.post('/', tweetController.createTweet);
+// json 형태로 입력 후 추가된 데이터까지 모두 json으로 출력
+// router.post('/', (req, res, next) => {
+//     const { text, name, username } = req.body;
+//     const tweet = {
+//         id: '10',
+//         text: text,
+//         createdAt: Date.now().toString(),
+//         name: name,
+//         username: username,
+//         url: 'https://www.logoyogo.com/web/wp-content/uploads/edd/2021/02/logoyogo-1-45.jpg'
+//     };
+//     tweets = [tweet, ...tweets];
+//     res.status(201).json(tweets);
+// });
+
+router.post('/', isAuth, tweetController.createTweet);
+
+
 
 // 트윗 수정하기
 // PUT
 // http://localhost:8080/tweets/:id
-// username, text
+// id, username, text
 // json 형태로 입력 후 변경된 데이터까지 모두 json으로 출력
-router.put('/', tweetController.updateTweet);
+// router.put('/:id', (req, res, next) => {
+//     const id = req.params.id;
+//     const text = req.body.text;
+//     const tweet = tweets.find((tweet) => tweet.id === id);
+//     if(tweet){
+//         tweet.text = text;
+//         res.status(201).json(tweet);
+//     }else{
+//         res.status(404).json({message: `${id}의 트윗이 없습니다`});
+//     }
+// });
+
+
+router.put('/', validateTweet, isAuth, tweetController.updateTweet);
+
+
 
 
 // 트윗 삭제하기
 // DELETE
 // http://localhost:8080/tweets/:id
-router.delete('/:id', tweetController.deleteTweet);
+// router.delete('/:id', (req, res, next) => {
+//     const id = req.params.id;
+//     tweets = tweets.filter((tweet) => tweet.id !== id);
+//     res.status(204)
+// });
+// export default router;
+
+router.delete('/', isAuth, tweetController.deleteTweet);
+
+
+export default tweetsRouter;
