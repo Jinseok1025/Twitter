8643da690f683b207e06e581a1fc935479977ce4
Jhin <54132604+Jinseok1025@users.noreply.github.com>
2024-05-21 08:49:42 +0900

    Add files via upload

---
+++ mysql/data/auth.js
@@ -0,0 +1,31 @@
+import { db } from '../db/database.js';
+
+// 아이디(username) 중복검사
+export async function findByUsername(username){
+    return db.execute('select * from users where username = ?', [username]).then((result) => {
+        console.log(result);
+        return result[0][0];
+    });
+}
+
+// id 중복검사
+export async function findById(id){
+    return db.execute('select * from users where id = ?', [id]).then((result) => {
+        console.log(result);
+        return result[0][0];
+    });
+}
+
+export async function createUser(user){
+    console.log(user);
+    const {username, hashed, name, email, url} = user;
+    return db.execute('insert into users (username, password, name, email, url) values (?, ?, ?, ?, ?)', [username, hashed, name, email, url]).then((result) => {
+        console.log(result);    // result[0].insertId
+        return result[0].insertId;
+    });
+}
+
+// export async function login(username){
+//     const user = users.find((user) => user.username === username)
+//     return user;
+// }
---
+++ mysql/data/tweet.js
@@ -0,0 +1,50 @@
+import { db } from '../db/database.js';
+
+const SELECT_JOIN = 'select tw.id, tw.text, tw.createdAt, tw.userId, us.username, us.name, us.email, us.url from tweets as tw join users as us on tw.userId = us.id';
+
+const ORDER_DESC = 'order by tw.createdAt desc';
+
+// 모든 트윗을 리턴
+export async function getAll() {
+    return db.execute(`${SELECT_JOIN} ${ORDER_DESC}`).then((result) => {
+        console.log(result);
+        return result;
+    });
+}
+
+// 해당 아이디에 대한 트윗을 리턴
+export async function getAllByUsername(username){
+    return db.execute(`${SELECT_JOIN} where username = ? ${ORDER_DESC}`, [username]).then((result) => {
+        console.log(result);
+        return result;
+    });
+}
+
+// 글번호에 대한 트윗을 리턴
+export async function getById(id){
+    return db.execute(`${SELECT_JOIN} where tw.id = ? ${ORDER_DESC}`, [id]).then((result) => {
+        console.log(result);
+        return result;
+    });
+}
+
+// 트윗을 작성
+export async function create(text, userId){
+    return db.execute('insert into tweets (text, userId) values (?, ?)', [text, userId]).then((result) => {
+        console.log(result);
+        return getById(result[0].insertId);
+    });
+}
+
+// 트윗을 변경
+export async function update(id, text){
+    return db.execute('update tweets set text = ? where id = ?', [text, id]).then((result) => {
+        console.log(result);
+        return getById(id);
+    });
+}
+
+// 트윗을 삭제
+export async function remove(id){
+    return db.execute('delete from tweets where id = ?', [id]);
+}
