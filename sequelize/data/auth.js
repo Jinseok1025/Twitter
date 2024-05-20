b396fbcf23dba9a9ea9b1995f5e8753377e57e52
Jhin <54132604+Jinseok1025@users.noreply.github.com>
2024-05-21 08:51:27 +0900

    Add files via upload

---
+++ sequelize/data/auth.js
@@ -0,0 +1,53 @@
+import SQ from 'sequelize';
+import { sequelize } from '../db/database.js';
+const DataTypes = SQ.DataTypes;
+
+export const User = sequelize.define(
+    'user',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        username: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING(150),
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        url: DataTypes.STRING(1000)
+    },
+    { timestamps: false }
+);
+
+
+// 아이디(username) 중복검사
+export async function findByUsername(username){
+    return User.findOne({where: {username}});
+}
+
+// id 중복검사
+export async function findById(id){
+    return User.findByPk(id);
+}
+
+export async function createUser(user){
+    return User.create(user).then((data) => data.dataValues.id)
+}
+
+// export async function login(username){
+//     const user = users.find((user) => user.username === username)
+//     return user;
+// }
---
+++ sequelize/data/tweet.js
@@ -0,0 +1,79 @@
+import SQ from 'sequelize';
+import { sequelize } from '../db/database.js';
+import { User } from './auth.js';
+
+const DataTypes = SQ.DataTypes;
+const Sequelize = sequelize;
+
+const INCLUDE_USER = {
+    attributes: [
+        'id',
+        'text',
+        'createdAt',
+        'userId',
+        [Sequelize.col('user.name'), 'name'],
+        [Sequelize.col('user.username'), 'username'],
+        [Sequelize.col('user.url'), 'url']
+    ],
+    include: {
+        model: User,
+        attributes: [],
+    }
+}
+
+const ORDER_DESC = {
+    order: [['createdAt', 'DESC']]
+}
+
+const Tweet = sequelize.define('tweet', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    text: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    }
+}, { timestamps: false });
+
+Tweet.belongsTo(User);
+
+
+// 모든 트윗을 리턴
+export async function getAll() {
+    return Tweet.findAll({ ...INCLUDE_USER, ...ORDER_DESC });
+}
+
+// 해당 아이디에 대한 트윗을 리턴
+export async function getAllByUsername(username){
+    return Tweet.findAll({ ...INCLUDE_USER, ...ORDER_DESC, include: {
+        ...INCLUDE_USER.include, where: {username}
+    } });
+}
+
+// 글번호에 대한 트윗을 리턴
+export async function getById(id){
+    return Tweet.findOne({ where: {id}, ...INCLUDE_USER });
+}
+
+// 트윗을 작성
+export async function create(text, userId){
+    return Tweet.create({ text, userId }).then((data) => this.getById(data.dataValues.id));
+}
+
+// 트윗을 변경
+export async function update(id, text){
+    return Tweet.findByPk(id, INCLUDE_USER).then((tweet) => {
+        tweet.text = text;
+        return tweet.save();
+    });
+}
+
+// 트윗을 삭제
+export async function remove(id){
+    return Tweet.findByPk(id).then((tweet) => {
+        tweet.destroy();
+    });
+}
